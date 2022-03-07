c5c16fbf1b9b13def8ec6da980cec51ac3d329c6
Isaac Brieske <ipbrieske@gmail.com>
2022-03-06 16:07:20 -0800

    Setting up Resources folder

--- PyPoll.py
+++ PyPoll.py
@@ -4,3 +4,13 @@
 #3. The percentage of votes each candidate won
 #4. The total number of votes each candidate won
 #5. The winner of the election based on popular vote
+
+import csv
+import os
+
+# Open the election results and read the file
+#Assign a variable for the file to load and the path
+fileToLoad = os.path.join("Resources", "election_reuslts.csv")
+# Open th eelction results and read the file
+with open(fileToLoad) as election_data:
+    print(election_data)

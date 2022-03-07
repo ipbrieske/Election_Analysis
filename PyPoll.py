19ed00b1ec1bc6f0211d3a40c7a73ea8ac42d87d
Isaac Brieske <ipbrieske@gmail.com>
2022-03-06 20:17:38 -0800

    3.4.5 Periodic save

--- PyPoll.py
+++ PyPoll.py
@@ -8,9 +8,17 @@
 import csv
 import os
 
-# Open the election results and read the file
-#Assign a variable for the file to load and the path
-fileToLoad = os.path.join("Resources", "election_reuslts.csv")
-# Open th eelction results and read the file
-with open(fileToLoad) as election_data:
-    print(election_data)
+# Create a file name variable to a direct or indirect path to the file
+fileToLoad = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+fileToWrite = os.path.join("analysis", "election_results.txt")
+
+# USing the open() function with the "w" mode we will write data to the file
+with open(fileToLoad) as electionData:
+    # To Do: read and analyze the data here
+    fileReader = csv.reader(electionData)
+
+    # Print Header row
+    headers = next(fileReader)
+    print(headers)
+    
